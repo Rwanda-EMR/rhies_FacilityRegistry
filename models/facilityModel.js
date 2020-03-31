9e5e80b2461f081383468a016eb6f4f665b9b889
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-03-31 16:59:32 +0000

    core of facility model

---
+++ models/facilityModel.js
@@ -0,0 +1,27 @@
+{
+    "fosaCode" : "",
+    "name" : "",
+    "url" : "",
+    
+    "geoPosition" : 
+        {   
+            "province" : "",
+            "district" : "",
+            "sector" : "",
+            "cell" : "",
+            "umudugudu" : ""
+        },
+    "openingDate" : "",
+    "closingDate" : "",
+    "email" : "",
+    "phoneNumber" : "",
+    "coordinate" : 
+        {
+            "longitude" : "",
+            "latitude" : "",
+            "altitude" : ""
+        },
+    
+    
+
+}
