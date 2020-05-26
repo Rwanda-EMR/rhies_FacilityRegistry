33c1c2f066aa9c1960bcbb4b1dffb7d24644d563
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-05-26 10:34:10 +0000

    Add db connection sharable object

---
+++ models/dbConnect.js
@@ -0,0 +1,19 @@
+const MongoClient = require( 'mongodb' ).MongoClient;
+const apiConfig = require('../server/config/config.json');
+const url = apiConfig.facilityregistry.mongodb.url;
+
+var _db;
+
+module.exports = {
+
+  connectToServer: function( callback ) {
+    MongoClient.connect( url,  { useNewUrlParser: true }, function( err, client ) {
+      _db  = client.db('FacilityRecord');
+      return callback( err );
+    } );
+  },
+
+  getDb: function() {
+    return _db;
+  }
+};
